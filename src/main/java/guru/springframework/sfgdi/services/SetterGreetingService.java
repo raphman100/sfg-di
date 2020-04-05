8d963bd72425e00ca00f73ad5f24182427a92012
Unknown <raphman100@gmail.com>
2020-04-04 22:51:38 -0400

    Used Qualifiers

--- src/main/java/guru/springframework/sfgdi/services/SetterGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterGreetingService.java
@@ -3,10 +3,11 @@ package guru.springframework.sfgdi.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl implements GreetingService {
+public class SetterGreetingService implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
-		return "Hello World from GreetingServiceImpl";
+		return "Hello World from Setter";
 	}
+
 }
